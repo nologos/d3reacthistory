28c21729beee6a2c81e2bef8d14431d735f3f872
nologos <git.nologos@outlook.com>
2024-09-03 00:12:07 +0100

    example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-// import { useState } from 'react'
+import React from "react"
+import ExampleComponent from "./components/ExampleComponent.tsx"
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <div className="container">
-      <button className="flex-1 bg-primary-900 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
-        Hello
-      </button>
+      <ExampleComponent />
     </div>
   )
 }
---
+++ src/components/ExampleComponent.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ToggleButton from './ToggleButton';
+
+
+const ExampleComponent = () => {
+
+
+    const [buttonState, setbuttonState] = useState(false);
+
+    const handleToggle = () => {
+        setbuttonState(!buttonState);
+    };
+
+    return (
+        <div className="container">
+            <ToggleButton handleToggle={handleToggle} buttonState={buttonState} />
+            <button className="flex-1 bg-primary-900 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
+                Hello
+            </button>
+        </div>
+    )
+}
+
+export default ExampleComponent;
