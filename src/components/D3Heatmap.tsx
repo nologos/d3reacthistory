16b8e97233dd04a2c64cdcdf1f4de8b1b879930e
nologos <git.nologos@outlook.com>
2024-09-06 22:57:22 +0100

    d3 item tooltip

--- src/components/D3Heatmap.tsx
+++ src/components/D3Heatmap.tsx
@@ -5,10 +5,7 @@ const LoadComponent: React.FC<any> = () => {
   const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<string | null>(null)
 
-  function bar(
-    hookcomponent = '.heatmap', // The CSS selector for the component to which the SVG will be attached.
-    csvpath = '/src/components/dji.csv', // The path to the CSV file containing the data.
-  ) {
+  function bar(hookcomponent = '.heatmap', csvpath = '/src/components/dji.csv') {
     // Set dimensions and size for the calendar heatmap
     const width = 960
     const height = 136
@@ -20,79 +17,108 @@ const LoadComponent: React.FC<any> = () => {
 
     // Set color scale mapping domain values to a range of classes (quantized)
     const color = d3.scaleQuantize()
-      .domain([-0.05, 0.05]) // Domain of the data (percentage change)
-      .range(d3.range(11).map((d) => { return `q${d}-11` })) // Classes for coloring
-
-    // Create an SVG element for each year in the range and append to the selected component
-    const svg = d3.select(hookcomponent).selectAll('svg')
-      .data(d3.range(2009, 2010)) // The range of years (only 2009 in this case)
-      .enter().append('svg') // Append an SVG for each year
-      .attr('width', width)
-      .attr('height', height)
-      .attr('class', 'RdYlGn') // Color scheme class
-      .append('g')
-      .attr('transform', `translate(${(width - cellSize * 53) / 2},${height - cellSize * 7 - 1})`) // Position the group element
-
-    // Append a text element for the year, rotated 90 degrees
-    svg.append('text')
-      .attr('transform', `translate(-6,${cellSize * 3.5})rotate(-90)`)
-      .style('text-anchor', 'middle')
-      .text((d) => { return d }) // Display the year
-
-    // Add month labels at the top of each column
-    svg.selectAll('.month')
-      .data(d => d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
-      .enter().append('text')
-      .attr('class', 'month-label')
-      .attr('x', d => (d3.timeWeek.count(d3.timeYear(d), d) * cellSize + cellSize / 2) + 40)
-      .attr('y', -5) // Position above the calendar
-      .style('text-anchor', 'middle')
-      .text(d => d3.timeFormat('%b')(d)) // Display the abbreviated month name
-
-    // Create a rectangle for each day of the year
-    const rect = svg.selectAll('.day')
-      .data((d) => { return d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1)) }) // Create an array of all days in the year
-      .enter().append('rect') // Append a rectangle for each day
-      .attr('class', 'day')
-      .attr('width', cellSize)
-      .attr('height', cellSize)
-      .attr('x', (d) => { return d3.timeWeek.count(d3.timeYear(d), d) * cellSize }) // Position based on the week number
-      .attr('y', (d) => { return d.getDay() * cellSize }) // Position based on the day of the week
-      .datum(format) // Attach the formatted date to each rectangle
-
-    // Add a tooltip (title) to each rectangle to show the date
-    rect.append('title')
-      .text((d) => { return d })
-
-    // Draw the outline of each month
-    svg.selectAll('.month')
-      .data((d) => { return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)) }) // Create an array of months
-      .enter().append('path')
-      .attr('class', 'month')
-      .attr('d', monthPath) // Use a custom function to define the path of each month
+      .domain([-0.05, 0.05])
+      .range(d3.range(11).map(d => `q${d}-11`))
 
     // Load the CSV data and process it
-    d3.csv(csvpath).then((poooop: any) => {
+    const data = d3.csv(csvpath).then((poooop: any) => {
       const data = d3.rollup(
         poooop,
-        v => (v[0].Close - v[0].Open) / v[0].Open, // Calculate the percentage change for each day
-        d => d.Date, // Group by date
+        v => (v[0].Close - v[0].Open) / v[0].Open,
+        d => d.Date,
       )
 
+      // Create an SVG element for each year in the range and append to the selected component
+      const svg = d3.select(hookcomponent).selectAll('svg')
+        .data(d3.range(2009, 2010))
+        .enter().append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('class', 'RdYlGn')
+        .append('g')
+        .attr('transform', `translate(${(width - cellSize * 53) / 2},${height - cellSize * 7 - 1})`)
+
+      // Append a text element for the year, rotated 90 degrees
+      svg.append('text')
+        .attr('transform', `translate(-6,${cellSize * 3.5})rotate(-90)`)
+        .style('text-anchor', 'middle')
+        .text((d) => { return d }) // Display the year
+
+      // Initialize Tooltip
+      const tooltip = d3.select(hookcomponent)
+        .append('div')
+        .attr('class', 'day2')
+        .style('opacity', 0)
+        .style('pointer-events', 'none')
+        .style('position', 'absolute')
+        .style('z-index', 9999)
+        .style('width', '250px')
+        .style('max-width', '250px')
+        .style('overflow', 'hidden')
+        .style('padding', '15px')
+        .style('font-size', '12px')
+        .style('line-height', '14px')
+        .style('color', 'rgb(51, 51, 51)')
+        .style('background', 'rgba(255, 255, 255, 0.75)')
+
+      // Add month labels at the top of each column
+      svg.selectAll('.month')
+        .data(d => d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
+        .enter().append('text')
+        .attr('class', 'month-label')
+        .attr('x', d => (d3.timeWeek.count(d3.timeYear(d), d) * cellSize + cellSize / 2) + 40)
+        .attr('y', -5) // Position above the calendar
+        .style('text-anchor', 'middle')
+        .text(d => d3.timeFormat('%b')(d)) // Display the abbreviated month name
+
+      // Create a rectangle for each day of the year
+      const rect = svg.selectAll('.day')
+        .data(d => d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
+        .enter().append('rect')
+        .attr('class', 'day')
+        .attr('width', cellSize)
+        .attr('height', cellSize)
+        .attr('x', d => d3.timeWeek.count(d3.timeYear(d), d) * cellSize)
+        .attr('y', d => d.getDay() * cellSize)
+        .datum(format)
+
+      // Add a tooltip (title) to each rectangle to show the date
+      rect.append('title')
+        .text(d => d)
+
+      // Handle mouse events for tooltips
+      rect.on('mouseover', (event: any, d) => {
+        if (!d)
+          return
+        const dataPoint = `${d}: ${percent(data.get(d))}`
+        tooltip.html(dataPoint)
+          .style('left', `${event.pageX + 10}px`)
+          .style('top', `${event.pageY - 20}px`)
+          .transition()
+          .duration(200)
+          .style('opacity', 1)
+      })
+        .on('mouseout', () => {
+          tooltip.transition()
+            .duration(500)
+            .style('opacity', 0)
+        })
+
+      // Draw the outline of each month
+      svg.selectAll('.month')
+        .data((d) => { return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1)) }) // Create an array of months
+        .enter().append('path')
+        .attr('class', 'month')
+        .attr('d', monthPath) // Use a custom function to define the path of each month
+
       // Apply the color scale to the rectangles based on the data
-      rect
-        .filter((d) => {
-          return data.has(d) // Filter out days without data
-        })
-        .attr('class', (d) => {
-          return `day ${color(data.get(d))}` // Apply the appropriate color class
-        })
+      rect.filter(d => data.has(d))
+        .attr('class', d => `day ${color(data.get(d))}`)
         .select('title')
-        .text((d) => {
-          return `${d}: ${percent(data.get(d))}` // Update the tooltip to show the date and percentage change
-        })
+        .text(d => `${d}: ${percent(data.get(d))}`)
+      return data
     }).catch((error: any) => {
-      console.error('Error loading the CSV file:', error) // Log any errors that occur during CSV loading
+      console.error('Error loading the CSV file:', error)
     })
 
     // Function to create the SVG path for each month's outline
@@ -147,9 +173,7 @@ const LoadComponent: React.FC<any> = () => {
   }
 
   if (!loading) {
-    return (
-      <div className="heatmap"></div>
-    )
+    return <div className="heatmap"></div>
   }
 
   return null
